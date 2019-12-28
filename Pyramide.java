3e3644a10814fe1e42d3c7e5427aa8c89801645b
Molmol92 <58810159+Molmol92@users.noreply.github.com>
2019-12-28 18:04:53 +0100

    pyramide avec deux méthodes

---
+++ Pyramide.java
@@ -0,0 +1,20 @@
+package projet1;
+
+public class Pyramide {
+
+	public static void main(String[] args) {
+
+		for (int i=0; i<5; i++) {
+			for(int j=0; j<5-i;j++) {
+			System.out.print(" ");
+		}
+		for (int k=0;k<=i; k++) {
+			System.out.print("x ");
+		}
+		System.out.println();
+	
+			}
+		
+		}
+		
+	}
